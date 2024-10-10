71ff34a1991b8c68ab1b3d1f5ddd0311e11b4b80
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-11 06:58:27 +0700

    added internet permission - done!

--- app/src/main/java/com/example/recipeapp/ApiService.kt
+++ app/src/main/java/com/example/recipeapp/ApiService.kt
@@ -5,11 +5,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 private val retrofit = Retrofit.Builder()
-    .baseUrl("www.themealdb.com/api/json/v1/1/")
+    .baseUrl("https://www.themealdb.com/api/json/v1/1/")
     .addConverterFactory(GsonConverterFactory.create())
     .build()
 
-val mealdbApi = retrofit.create(ApiService::class.java)
+val mealdbApi: ApiService = retrofit.create(ApiService::class.java)
 
 interface ApiService {
     @GET("categories.php")
