2d20d914323ca23cbc3bf7f001147e103d59d893
Rafly Yanuar <muhammad.rafly02@ui.ac.id>
2024-10-11 06:27:11 +0700

    added MainViewModel.kt

--- app/src/main/java/com/example/recipeapp/Category.kt
+++ app/src/main/java/com/example/recipeapp/Category.kt
@@ -7,7 +7,6 @@ data class Category(
     val strCategoryDescription: String,
 )
 
-
 data class CategoriesResponse(
     val categories: List<Category>
 )
---
+++ app/src/main/java/com/example/recipeapp/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.recipeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val _categoriesState = mutableStateOf(CategoriesState())
+    val categoriesState : State<CategoriesState> = _categoriesState
+
+    init {
+        fetchCategories()
+    }
+
+    private fun fetchCategories() {
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    categories = response.categories,
+                    error = null
+                )
+            }
+            catch (e: Exception) {
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    error = "ERROR FETCHING CATEGORIES: ${e.message}",
+                )
+            }
+        }
+    }
+
+    data class CategoriesState(
+        val loading: Boolean = true,
+        val categories: List<Category> = emptyList(),
+        val error: String? = null
+    )
+}
